50b447ab3d889221f658d8a00c42950ac872ab47
patrick-rodriguez <110691337+patrick-rodriguez@users.noreply.github.com>
2023-11-04 19:55:49 -0700

    Update library.py

--- library.py
+++ library.py
@@ -256,3 +256,26 @@ customer_transformer = Pipeline(steps=[
 # fitted_pipeline = titanic_transformer.fit(X_train, y_train)  #notice just fit method called
 # import joblib
 # joblib.dump(fitted_pipeline, 'fitted_pipeline.pkl')
+
+def dataset_setup(original_table, label_column_name:str, the_transformer, rs, ts=.2):
+  #your code below
+  original_features = original_table.drop(columns=label_column_name)
+  labels = original_table[label_column_name].to_list()
+  X_train, X_test, y_train, y_test = train_test_split(original_features, labels, test_size=ts, shuffle=True,
+                                                    random_state=rs, stratify=labels)
+
+  X_train_transformed = the_transformer.fit_transform(X_train, y_train)
+  X_test_transformed = the_transformer.transform(X_test)
+
+  X_train_numpy = X_train_transformed.to_numpy()
+  X_test_numpy = X_test_transformed.to_numpy()
+  y_train_numpy = np.array(y_train)
+  y_test_numpy = np.array(y_test)
+
+  return X_train_numpy, X_test_numpy, y_train_numpy, y_test_numpy
+
+def titanic_setup(titanic_table, transformer=titanic_transformer, rs=titanic_variance_based_split, ts=.2):
+  return dataset_setup(titanic_table, 'Survived', transformer, rs=rs, ts=ts)
+
+def customer_setup(customer_table, transformer=customer_transformer, rs=customer_variance_based_split, ts=.2):
+  return dataset_setup(customer_table, 'Rating', transformer, rs=rs, ts=ts)
