445e6b9116ae467af61cbf843fd84a81b4eca0c6
patrick-rodriguez <110691337+patrick-rodriguez@users.noreply.github.com>
2023-11-18 13:17:55 -0800

    Update library.py

--- library.py
+++ library.py
@@ -7,7 +7,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.impute import KNNImputer
 from sklearn.metrics import f1_score
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, HalvingGridSearchCV
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 
 
